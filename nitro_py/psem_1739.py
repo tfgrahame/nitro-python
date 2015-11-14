05281b966b229c09bd365bba8a47e890e7ab6076
Tom Grahame <tfgrahame@gmail.com>
2015-11-14 16:53:39 +0000

    Use live

--- nitro_py/psem_1739.py
+++ nitro_py/psem_1739.py
@@ -5,7 +5,7 @@ import os
 def main():
     mixins = ['availability', 'ancestor_titles', 'versions_availability', 'genre_groupings']
     filters = {'entity_type':'clip', 'availability':'available', 'media_type':'audio', 'availability_entity_type':'clip', 'page_size':'100', 'master_brand':'bbc_radio_four','genre':'100005'}
-    base = NITRO_E2E + 'programmes'
+    base = NITRO_LIVE + 'programmes'
     call_nitro(base, mixins, filters)
 
 if __name__ == "__main__":
